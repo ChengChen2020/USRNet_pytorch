0550c92cc32f6ea2a40753b83bd6b96745bd31f0
ChengChen2020 <cc6858@nyu.edu>
2022-10-10 21:49:00 -0400

    update

--- hpc.sh
+++ hpc.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name=usrnet
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=12:00:00
-#SBATCH --mem=70GB
+#SBATCH --time=48:00:00
+#SBATCH --mem=50GB
 #SBATCH --gres=gpu:rtx8000:1
 #
 #SBATCH --mail-type=END
